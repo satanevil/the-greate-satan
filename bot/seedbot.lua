53b57e323bc953a67d388ed344212f2ec469fc4a
satanevil <satanevil150@gmail.com>
2016-02-04 20:10:29 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,19 +226,17 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {144132967,139763200,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[The great Satan
 
-https://github.com/SEEDTEAM/TeleSeed
+
+
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@BI_MOKH021 [Founder]
+@tnt_boy1 [Manager]
 
 Special thanks to
 awkward_potato
